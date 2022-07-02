6188b26218d3b7d3ce1a28da49b4e83942936015
Phạm Quang Linh <77527768+phamquanglinhdev@users.noreply.github.com>
2022-07-02 18:01:47 +0700

    Update UsersSeeder.php

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -24,37 +24,5 @@ class UsersSeeder extends Seeder
             'role' => 0,
             'created_at' => now(),
         ]);
-
-        DB::table('users')->insert([
-            'name' => 'duy',
-            'email' => 'vanduy@gmail.com',
-            'password' => Hash::make('password'),
-            'role' => 0,
-            'created_at' => now(),
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'Phạm Quang linh',
-            'email' => 'quanglinh@gmail.com',
-            'password' => Hash::make('password'),
-            'role' => 0,
-            'created_at' => now(),
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'giang',
-            'email' => 'truonggiang@gmail.com',
-            'password' => Hash::make('password'),
-            'role' => 0,
-            'created_at' => now(),
-        ]);
-
-        DB::table('users')->insert([
-            'name' => 'tuc',
-            'email' => 'vantuc@gmail.com',
-            'password' => Hash::make('password'),
-            'role' => 0,
-            'created_at' => now(),
-        ]);
     }
 }
